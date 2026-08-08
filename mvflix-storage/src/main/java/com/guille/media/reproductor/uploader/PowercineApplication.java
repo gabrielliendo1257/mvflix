e9190b642aa3f707079b219278f20eab3ff4acf5
Gabriel L. <gabrielliendo1257@gmail.com>
2026-08-08 14:55:27 -0500

    refactor(storage): reemplazar Feign por WebClient reactivo para llamadas al user-service
    El stack es WebFlux/R2DBC, pero la llamada machine-to-machine hacia
    user-service usaba Feign + OkHttp (I/O bloqueante: roba hilos del event loop
    de Reactor).
    
    - eliminar UserServiceFeignClient, FeignAuthenticationInterceptor,
      OAuth2ClientConfiguration (servlet) y HttpClientConfiguration (RestClient muerto)
    - nuevo OAuth2WebClientConfiguration: WebClient + filtro
      ServerOAuth2AuthorizedClientExchangeFilterFunction con client_credentials;
      token cache propio porque UnAuthenticatedServerOAuth2AuthorizedClientRepository
      fue removido en Spring Security 6.4
    - nuevo UserServiceWebClientAdapter implementando UserServiceCommandPort
      (el puerto de dominio no cambia)
    - pom: quitar spring-cloud-openfeign + BOM + okhttp explicito
    - remover @EnableFeignClients y la property ociosa client-principal

--- mvflix-storage/src/main/java/com/guille/media/reproductor/uploader/PowercineApplication.java
+++ mvflix-storage/src/main/java/com/guille/media/reproductor/uploader/PowercineApplication.java
@@ -3,11 +3,9 @@ package com.guille.media.reproductor.uploader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableCaching
-@EnableFeignClients
 @EnableScheduling
 @SpringBootApplication
 public class PowercineApplication {
